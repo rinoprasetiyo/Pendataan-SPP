2f29f9ce866035c368ddf14b8791ef67533550e5
rinoprasetiyo <156102092+rinoprasetiyo@users.noreply.github.com>
2025-07-07 09:10:45 +0700

    Add files via upload

---
+++ table_siswa.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `siswa` (
+  `nisn` char(10) NOT NULL,
+  `nis` char(8) NOT NULL,
+  `nama` varchar(35) NOT NULL,
+  `id_kelas` int(11) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_telp` varchar(13) NOT NULL,
+  `id_spp` int(11) NOT NULL
+)
